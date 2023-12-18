4b2d6bf24f1b5d279d3dccdf6f22dbb8ddfea8a5
YoussefHussein0 <120174582+YoussefHussein0@users.noreply.github.com>
2023-12-18 18:23:21 +0200

    Add files via upload
    new js file after fixing a bug

--- Assignment 10 - Bookmarker/js/main.js
+++ Assignment 10 - Bookmarker/js/main.js
@@ -6,6 +6,8 @@ const siteNameInput = document.getElementById("siteName"),
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
   regex = new RegExp(regURL);
 
+var arrayOfSites = [];
+
 if (localStorage.getItem("Bookmarks") != null) {
   arrayOfSites = JSON.parse(localStorage.getItem("Bookmarks"));
   showBookmark();
